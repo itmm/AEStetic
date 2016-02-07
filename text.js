8f82a89feb330390574b98aea43269c520a5a599
Timm Knape <timm@kna-st.de>
2016-02-07 13:52:10 +0100

    removed alert

--- text.js
+++ text.js
@@ -89,7 +89,6 @@ window.addEventListener('load', function () {
         } else if (c >= 48 && c <= 57 || c >= 65 && c <= 70 || c >= 97 && c <= 102) {
             // hex digits
         } else {
-            alert('code ' + c);
             evt.preventDefault();
         }
     });
